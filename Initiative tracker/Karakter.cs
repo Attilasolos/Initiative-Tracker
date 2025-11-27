1b099594346f5b557f8661851e49de6eec19d8aa
Zsigmond Attila <zsigmond.attila.laszlo@students.jedlik.eu>
2025-11-27 12:11:28 +0100

    Karakterek staitisztikáinak kiírása

--- Initiative tracker/Karakter.cs
+++ Initiative tracker/Karakter.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Navigation;
 
 namespace Initiative_tracker
 {
@@ -12,26 +15,27 @@ namespace Initiative_tracker
         public int HP { get; set; }
         public int Bonusz { get; set; }
         public int Rendezesi_Ertek { get; set; }
-        public bool Akcio {  get; set; }
+        public bool Akcio { get; set; }
         public bool Bonusz_Akcio { get; set; }
-        public bool Reakcio {get; set;}
-        public DateTime Szuletesi_Datum {  get; set; }
+        public bool Reakcio { get; set; }
+        public DateTime Szuletesi_Datum { get; set; }
         Random rnd = new Random();
-        public Karakter(string Sor) 
+
+
+        public Karakter(string Sor)
         {
-            
             var Data = Sor.Split(';');
             Nev = Data[0];
             HP = int.Parse(Data[1]);
             Bonusz = int.Parse(Data[2]);
-           
+
             if (int.Parse(Data[3]) == 0)
             {
                 Akcio = false;
             }
             else
             {
-                Akcio=true;
+                Akcio = true;
             }
             if (int.Parse(Data[4]) == 0)
             {
@@ -50,10 +54,22 @@ namespace Initiative_tracker
                 Reakcio = true;
             }
             Szuletesi_Datum = DateTime.Parse(Data[6]);
-        }
-        public void Sorba_Rakás()
-        {
+
             Rendezesi_Ertek = rnd.Next(1, 20);
         }
+
+
+        public static List<Karakter> Read(string filename = "karakterek.txt") {
+            var list = new List<Karakter>();
+            using (var sr = new StreamReader(filename))
+            {
+                while (!sr.EndOfStream) {
+                    var row = sr.ReadLine()!;
+                    var karakter = new Karakter(row);
+                    list.Add(karakter);
+                }
+                }
+            return list;
+            }
     }
 }
--- Initiative tracker/MainWindow.xaml.cs
+++ Initiative tracker/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -16,9 +18,69 @@ namespace Initiative_tracker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Karakter> karakterek = new List<Karakter>();
         public MainWindow()
         {
             InitializeComponent();
+            
+            karakterek = Karakter.Read(); 
+            
+            foreach(var k in karakterek)
+            {
+                lstbx1.Items.Add(k.Nev);
+            }
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int Index = lstbx1.SelectedIndex;
+            Nmbx.Text = $"Name: {karakterek[Index].Nev}";
+            Hpbx.Text = $"HP: {karakterek[Index].HP}";
+            if (karakterek[Index].Akcio == true)
+            {
+                ACCB.IsChecked = true  ;
+            }
+            else
+            {
+                ACCB.IsChecked = false ;
+            }
+            if (karakterek[Index].Bonusz_Akcio == true)
+            {
+                BACB.IsChecked = true;
+            }
+            else
+            {
+                BACB.IsChecked = false;
+            }
+            if (karakterek[Index].Reakcio == true)
+            { 
+                RCB.IsChecked = true ;
+            }
+            else
+            {
+                RCB.IsChecked = false;
+            }
+            BTDP.SelectedDate = karakterek[Index].Szuletesi_Datum;
+
+            Bbx.Text = $"Bónusz: {karakterek[Index].Bonusz}";
+
+            Ibx.Text = $"Initiative: {karakterek[Index].Rendezesi_Ertek}";
+        }
+
+        private void ACCB_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BACB_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RCB_Checked(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
